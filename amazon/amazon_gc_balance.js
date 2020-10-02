a1144f78a36235812cdf6fa9cc8b0253bfbe2030
saiyedaws <saiyedaws@gmail.com>
2020-10-02 04:50:37 -0400

    update

--- amazon/amazon_gc_balance.js
+++ amazon/amazon_gc_balance.js
@@ -35,9 +35,51 @@ function getSafeGiftCardLimit(){
     var giftCardBalance = document.querySelector("#gc-ui-balance-gc-balance-value").innerText;
    
     var giftCardBalanceDecimal = giftCardBalance.match(/\d+\.?\d*/gi)[0];
+    var pendingTotal = getPendingTotal();
 
-    var usableGcLimit = giftCardBalanceDecimal*0.45;
+    var giftCardPlusPending = giftCardBalanceDecimal + pendingTotal;
 
+    var usableGcLimit = giftCardPlusPending*0.45;
     usableGcLimit = usableGcLimit.toFixed(2);
+
+    
+
     return usableGcLimit;
+}
+
+
+function getPendingTotal()
+{
+    var totalPendingAmount = 0;
+
+    var trElements = document.querySelectorAll("tr");
+   
+  
+    for (let index = 0; index < trElements.length; index++) {
+        var trElement = trElements[index];
+
+        if(trElement.textContent.includes('Pending'))
+        {
+            // something
+            console.log(trElement.innerText);
+
+            var price = trElement.innerText.match(/\d{1,3}[.](\d{1,2})/gi)[0];
+            price = Number(price);
+
+            console.log("price",price);
+
+            totalPendingAmount = totalPendingAmount+price;
+           
+
+            console.log("totalPendingAmount",totalPendingAmount);
+
+        }
+        
+    }
+
+
+    totalPendingAmount = totalPendingAmount.toFixed(2);
+    console.log("totalPendingAmount",totalPendingAmount);
+
+    return totalPendingAmount;
 }
